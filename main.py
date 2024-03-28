2001938e06547105959881ad4af16a4ff417a25c
tesseract <tesseractmaks@gmail.com>
2024-03-28 15:52:00 +0800

    Add read_msgs()

--- main.py
+++ main.py
@@ -1,66 +1,69 @@
 import argparse
 import asyncio
-import gui
+import time
 
 from os import getenv
-from pathlib import Path
+
+import gui
+from config import sender_log, reader_log, OpenConnection
 
 
-OUT_PATH = (Path(__file__).parent / "chat.log").absolute()
 HOST_CLIENT = str(getenv("HOST_CLIENT", "188.246.233.198"))
 PORT_CLIENT = int(getenv("PORT_CLIENT", 5000))
 
-
-async def generate_msgs(messages_queue):
-    messages_queue.put_nowait('Иван: Привет всем в этом чатике!')
-    return messages_queue
-
-
-async def draw_gui(messages_queue):
-    loop = asyncio.get_event_loop()
-    # messages_queue = asyncio.Queue()
-    sending_queue = asyncio.Queue()
-    status_updates_queue = asyncio.Queue()
-
-    loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+sending_queue = asyncio.Queue()
+status_updates_queue = asyncio.Queue()
+messages_queue = asyncio.Queue()
 
 
-def get_arguments():
-    parser = argparse.ArgumentParser(description="The code run chat.")
+async def read_msgs(messages_queue, host, port):
+    async with OpenConnection(host, port) as (reader, writer):
+        try:
+            while True:
+                data = await reader.readline()
+                messages_queue.put_nowait(data.decode())
+        except (ConnectionRefusedError, ConnectionResetError, ConnectionError) as exc:
+            reader_log.error(exc)
+            writer.close()
+            await writer.wait_closed()
+
+
+def argparser():
+
+    parser = argparse.ArgumentParser(description="Chat client")
+
     parser.add_argument(
-        "-ph", "--path", type=str, default=OUT_PATH, help="Set path to catalog use arguments: '--path'"
+        "-ht",
+        "--host",
+        type=str,
+        # default=str(getenv("CHAT_HOST", "minechat.dvmn.org")),
+        default=str(getenv("HOST_CLIENT", "188.246.233.198")),
+        help="Enter host",
     )
     parser.add_argument(
-        "-ht", "--host", type=str, default=HOST_CLIENT, help="Enter host use arguments: '--host'"
+        "-p",
+        "--port",
+        type=int,
+        # default=int(getenv("CHAT_PORT", 5050)),
+        default=int(getenv("PORT_CLIENT", 5000)),
+        help="Enter port",
     )
-    parser.add_argument(
-        "p", "--port", type=int, default=PORT_CLIENT, help="Enter port use argument: '--port' set number"
-    )
-    args = parser.parse_args()
-    return args.path, args.host, args.port
+    parser.add_argument("-t", "--token", type=str, help="Enter hash token")
+    parser.add_argument("-r", "--reg", type=str, help="Enter nickname for registration")
+    # parser.add_argument("msg", type=str, help="Enter message")
+    return parser.parse_args()
 
 
 async def main():
-
-    sending_queue = asyncio.Queue()
-    status_updates_queue = asyncio.Queue()
-    tasks = []
-    messages_queue = asyncio.Queue()
-    # generate = await generate_msgs(messages_queue)
-
-    for _ in range(5):
-        await asyncio.sleep(3)
-        tasks.append(generate_msgs(messages_queue))
+    parser = argparser()
+    host = parser.host
+    port = parser.port
+    tasks = [gui.draw(messages_queue, sending_queue, status_updates_queue), read_msgs(messages_queue, host, port)]
 
     await asyncio.gather(*tasks)
 
-    await gui.draw(messages_queue, sending_queue, status_updates_queue)
-
 
 if __name__ == "__main__":
-    # messages_queue = asyncio.Queue()
-    # draw_gui(messages_queue)
-
     asyncio.run(main())
 
 
